b6c830993f3fbef8f543ef59ffd626bcc22a3e8c
TanuSingal <singaltanisha0@gmail.com>
2025-01-27 01:25:02 +0530

    Fixed News file

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import InfiniteScroll from 'react-infinite-scroll-component';
 import Spinner from './Spinner';
 import NewsItem from './NewsItem';
